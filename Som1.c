6d4eff4a81292ee7fb64a001d5c516e3a66dff74
kike <enriraul.rdz@gmail.com>
2015-03-31 20:00:34 -0600

    Primeras ideas

--- Som1.c
+++ Som1.c
@@ -1,5 +1,17 @@
-/*Anexo de una modificacion.*/
+
 #include <stdio.h>
+struct node {
+	float *Y;
+	int posY;
+	float weight;
+	int hits;
+}
+
+struct network{
+	int posX;
+	struct node *nodes;
+	struct network *next;
+}
 
 int main(int argc, char **argv)
 {
@@ -7,7 +19,18 @@ int main(int argc, char **argv)
 	return 0;
 }
 
-float DistanciaEuclidiana(){
+void clearWeigth(){
 	
 	
 }
+
+float DistanciaEuclidiana(float *YNodo,float Yentrada,int n){
+	int i;
+	float DistEucli=0;
+	for(i=0;i<n;i++){
+		DistEucli=+ (YNodo[i] - Yentrada[i])*(YNodo[i] - Yentrada[i]);
+	}
+	DistEucli=sqrt(DistEucli);
+}
+
+
